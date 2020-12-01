db6d70f2aefbfde18a5375904d5d2f82c92d16ea
Gunyoung Yoon <me@dry8r3ad.com>
2020-12-01 17:19:50 +0900

    Initial Commit w/ Code

---
+++ src/main/java/kr/ac/inha/webAccountBook/WebAccountBookApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.webAccountBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAccountBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/inha/webAccountBook/WebAccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.inha.webAccountBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
